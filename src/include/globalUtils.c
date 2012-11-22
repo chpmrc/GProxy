b0ba8f57230827518c4f299e5b2a7234adde683d
Marco <mardurhack@gmail.com>
2012-11-22 23:04:38 +0100

    Just a bridget between tcp:59000 and udp:61000 (should be the ritardatore)

---
+++ src/include/globalUtils.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <features.h>
+
+#define TRUE 1
+#define FALSE 0
+#define SOCKERROR -1
+
+int sharedError; /* errno-like variable */
+char *logFilePath = "./log.txt"; /* Log is also saved to a file */
+int firstExecution = TRUE; /* At the beginning do something, then something else */
+
+/**
+ * @param {int} type
+ * @return {int}
+ * Questa funzione si occupa di ritornare un socket descriptor 
+ * creato a seconda del tipo dato come argomento (DGRAM o STREAM)
+ */
+int getSocket(int type){
+	int sockfd;
+	int trueOpt = 1;
+	sockfd = socket(AF_INET, type, 0);
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (int *)&trueOpt, sizeof(trueOpt)) == SOCKERROR){
+		return -1;
+	}
+	return sockfd;
+}
+
+/**
+ * @param {char *} ipAddr
+ * @param {short int} port
+ * @return {struct sockaddr}
+ * Questa funzione si occupa di ritornare un socket descriptor 
+ * creato a seconda del tipo dato come argomento (DGRAM o STREAM).
+ * Ritorna una struct sockaddr_in. address è una stringa in 
+ * dotted notation (es. "127.0.0.1").
+ */
+struct sockaddr_in getSocketAddress(char *address, unsigned short int port){
+	struct sockaddr_in newAddr;
+	memset(&newAddr, 0, sizeof(newAddr));
+	newAddr.sin_family = AF_INET;
+	if (!inet_aton(address, &newAddr.sin_addr)){
+		sharedError = TRUE;
+	}
+	newAddr.sin_port = htons(port);
+	sharedError = FALSE;
+	return newAddr;
+}
+
+/**
+ * @param {char *} filePath The path to the log file
+ */
+void logAppend(char* string){
+	FILE *debug = fopen(logFilePath, "a+");
+	if (firstExecution){
+		char currentDate[1000];
+		time_t instant = time(NULL);
+		/* Viene sempre usata la stessa locazione di memoria ma non ci interessa */
+		strftime(currentDate, 1000, "%d/%m/%y - %H:%M:%S", localtime(&instant));
+		firstExecution = FALSE;
+		fprintf(debug, "%s%s%s", "\n\n==================  ", currentDate ," ==================\n\n");
+	}
+	fprintf(debug, "%s", string);
+	fclose(debug);
+}
+
+/**
+ * @param {char *} message
+ * Questa funzione stampa un messaggio di errore e termina l'esecuzione
+ * sfruttando la funzione perror per stampare anche il messaggio relativo
+ * al valore di errno.
+ */
+void printError(char *message){
+	char msg[500];
+	char errstr[200];
+	sprintf(errstr, "%s", strerror(errno));
+	if (errno == 0){
+		sprintf(errstr, "%s", "This was not an OS related error so no errno value was set!");
+	}
+	sprintf(msg, "%s%s\n\t%s\n", "[ERROR] ", message, errstr);
+	printf("%s", msg);
+	logAppend(msg);
+	exit(1);
+}
+
+/**
+ * @param {char *} message
+ * Questa funzione stampa un messaggio di log sullo stdout.
+ */
+void printLog(char *message){
+	char msg[1000];
+	sprintf(msg, "%s%s\n", "[LOG] ", message);
+	printf("%s", msg);
+	logAppend(msg);
+}
--- src/psender.c
+++ src/psender.c
@@ -8,25 +8,37 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/select.h>
-#include "include/globalUtils.h"
+#include "include/globalUtils.c"
 
+#define PAYLOAD_SIZE 65510 /* Computed as IP_total_size - IP_max_header_size - id_size - type_size = 65535 - 20 - 4 - 1 */
+
+/* MAIN STUFF */
 struct sockaddr_in to, from, source, sourceClient; /* Indirizzo del socket locale e remoto del ritardatore e del sender */ 
 int ritardatore, sender, connectedSender; /* socket descriptor verso/da il ritardatore e dal sender */
 char sourceAddress[] = "0.0.0.0"; unsigned short int sourcePort = 59000;
-char fromAddress[] = "0.0.0.0"; /* INADDR_ANY */ unsigned short int fromPort = 60000;
+char fromAddress[] = "0.0.0.0"; unsigned short int fromPort = 60000;
 char toAddress[] = "127.0.0.1"; unsigned short int toPort = 61000;
 
+/* DATA STRUCTURES */
+typedef struct Pkt {
+	unsigned int id;
+	unsigned char type;
+	char body[PAYLOAD_SIZE];
+} Pkt;
+
+
 /* UTILS */
-char recvBuffer[1000], sendBuffer[1000];
+extern char *logFilePath;
+char recvBuffer[PAYLOAD_SIZE], sendBuffer[PAYLOAD_SIZE];
 int trueOpt = 1;
 int sharedError; /* Variabile di riconoscimento errori condivisa, serve per quelle funzioni che non ritornano interi */
-int readCounter, sentCounter;
+size_t readCounter, sentCounter;
 socklen_t toLen = sizeof(to);
 socklen_t sourceClientLen = sizeof(sourceClient);
 
-/* SELECT */
-fd_set canRead, canWrite, canExcept;
-struct timeval timeout;
+/* SELECT RELATED */
+fd_set canRead, canWrite, canExcept, canReadCopy, canWriteCopy, canExceptCopy;
+struct timeval timeout = { 2, 0 };
 int selectResult;
 int maxFd;
 
@@ -72,6 +84,7 @@ int main(){
 	
 	/* Lego il socket dal ritardatore ad un indirizzo in ricezione */
 	bind(ritardatore, (struct sockaddr *)&from, sizeof(from));
+	
 	/* Lego il socket dal sender ad un indirizzo in ricezione */
 	bind(sender, (struct sockaddr *)&source, sizeof(source));
 	listen(sender, 1);
@@ -81,52 +94,75 @@ int main(){
 	if (connectedSender < 0){
 		printError("There was an error with the accept(). See details below:");
 	}
+	printLog("Sender connected! Waiting for data...");
+	
+	/* Add the sockets into the sets of descriptors */
+	FD_ZERO(&canRead); FD_ZERO(&canWrite); FD_ZERO(&canExcept);
+	FD_SET(sender, &canRead); 
+	FD_SET(sender, &canExcept);
+	FD_SET(ritardatore, &canRead);
+	FD_SET(ritardatore, &canWrite);
+	FD_SET(ritardatore, &canExcept);
+	maxFd = (sender < ritardatore)? ritardatore : sender;
 	
 	/* Leggo i datagram */
 	while(TRUE){
-		/* Add the sockets into the sets of descriptors */
-		FD_ZERO(&canRead); FD_ZERO(&canWrite); FD_ZERO(&canExcept);
-		FD_SET(sender, &canRead); 
-		FD_SET(sender, &canExcept);
-		FD_SET(ritardatore, &canRead);
-		FD_SET(ritardatore, &canWrite);
-		FD_SET(ritardatore, &canExcept);
-		maxFd = (sender < ritardatore)? ritardatore : sender;
-		
+		/* Make a copy of the fd_set to avoid modifying the original one */
+		canReadCopy = canRead;
+		canWriteCopy = canWrite;
+		canExceptCopy = canExcept;
 		/* Main (and only) point of blocking from now on */
-		selectResult = select(maxFd+1, &canRead, &canWrite, &canExcept, NULL);
+		selectResult = select(maxFd+1, &canReadCopy, &canWriteCopy, &canExceptCopy, NULL);
 		
-		/* First of all check for errors on sockets */
+		/* First of all check for errors on sockets
 		if (FD_ISSET(sender, &canExcept)){
 			printError("There was an error with the sender socket!");
 		}
 		if (FD_ISSET(ritardatore, &canExcept)){
 			printError("There was an error with the ritardatore socket!");
 		}
+		*/
 		
 		/* Now check for I/O availability */
 		if (FD_ISSET(sender, &canRead)){
-			
+			printLog("Receiving data from the sender");
+			/* Here we have to build N packets and put them into a list, then continue the execution */
+			readCounter = recv(connectedSender, recvBuffer, 65000, MSG_DONTWAIT);
+			printf("Read %d bytes!\n", readCounter);
+			if (readCounter == 0){
+				close(sender);
+			}
 		}
 		if (FD_ISSET(ritardatore, &canRead)){
-			
+			printLog("Receiving data from the ritardatore");
+			/* Here we have to verify if an ICMP has been received and
+			 * search the id into the "sent" list. Once we find the guilty
+			 * packet we just put it again into the "toSend" list and switch port.
+			 * If an ACK is received the packet is removed from the "sent" list. */
 		}
 		if (FD_ISSET(ritardatore, &canWrite)){
-			
+			printLog("Sending data to the ritardatore");
+			/* Here we have to send as much packets as we can and move them into a "sent" list */
+			sentCounter = sendto(ritardatore, recvBuffer, readCounter, 0, (struct sockaddr *)&to, sizeof(to));
+			/* PROBLEMA: I byte trasmessi sono ok ma per qualche motivo il file non è lo stesso. Che sia la memset? */
+			printf("Sent %d bytes!\n", sentCounter);
+			if (sentCounter != 0){
+				memset(recvBuffer, 0, readCounter);
+			} else {
+				close(ritardatore);
+				printLog("ALL DONE!");
+				exit(0);	
+			}
 		}
 		
-		
-		
-		
-		
-		
-		
-		
 		continue;
+		
+		
 		/* RICEZIONE TCP */
 		readCounter = recv(connectedSender, recvBuffer, 1000, 0);
 		printf("reading!\n");
 		printf("[MGS_TCP]: %s", recvBuffer);
+		memset(recvBuffer, 0, strlen(recvBuffer));
 		exit(0);
 		memset(recvBuffer, 0, strlen(recvBuffer));
 		sleep(3);
